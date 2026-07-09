f187f55c63b2adacf5419aca6f69fe5666ca5666
Maks Solomon <maks.solomon2@gmail.com>
2026-07-09 22:45:02 +0200

    hero section1

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,6 +2,15 @@ const config = {
   plugins: {
     "@tailwindcss/postcss": {},
   },
+
+  extend: {
+    colors: {
+      black:{
+        DEFAULT: '#000000',
+        100: '#000319',
+      }
+    }
+  }
 };
 
 export default config;
