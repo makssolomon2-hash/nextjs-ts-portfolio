21641e48613d7bcfaf2fd1a53274e3b1e9f23d5c
Maks Solomon <maks.solomon2@gmail.com>
2026-07-18 22:35:41 +0200

    bento grid implemation attemnt

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,8 @@
-import React from 'react'
 import {Spotlight} from "@/components/ui/Spotlight";
 import { cn } from "@/lib/utils";
 import {TextGenerateEffect} from "@/components/ui/TextGenerateEffect";
 import MagicButton from './ui/MagicButton';
 import { FaLocationArrow } from 'react-icons/fa';
-import { FloatingNav } from './ui/FloatingNav';
 
 const Hero = () => {
     return (
@@ -12,11 +10,11 @@ const Hero = () => {
         <div className=" w-full w-9/12 pt-36 -z-10">
                 <div>
                     <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white"/>
-                    <Spotlight className="top[-200]- left-full h-[80vh] w-[50vw]" fill="purple"/>
-                    <Spotlight className="top-[100] left-180  h-[80vh] w-[20vw]" fill="blue"/>
+                    <Spotlight className="-top-40 left-full h-[80vh] w-[50vw]" fill="purple"/>
+                    <Spotlight className="top-24 left-44 h-[80vh] w-[20vw]" fill="blue"/>
                     <h1 className=" text-white w-full"> </h1>
                 </div>
-                <div className="hero-content flex  items-center justify-center  dark:bg-black/[0.03] h-screen w-full mt-[-150]  ">                 
+                <div className="hero-content flex  h-screen w-full -mt-36 items-center justify-center dark:bg-black/[0.03]">                 
                         <div
                             className={cn(
                                 "absolute inset-0",
@@ -26,10 +24,10 @@ const Hero = () => {
                             )}
                         />
                         <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black/[0.03]  "></div>
-                        <div className="flex justify-center pt-0  z-2 ">
-                            <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center a '>
+                        <div className="z-10 flex justify-center pt-0">
+                            <div className='flex max-w-[89vw] flex-col items-center justify-center md:max-w-2xl lg:max-w-[60vw]'>
                                
-                                <h2 className="uppercase tracking-widest  text-xs text-center text-blue-100 max-w-80 z-1">
+                                <h2 className="max-w-80 text-center text-xs uppercase tracking-widest text-blue-100">
                                     Dynamic Web Magic with Next.js {/* Add GSAP aniumatoons on text */}
                                 </h2>
 
@@ -39,7 +37,7 @@ const Hero = () => {
                                 />
 
                                 <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl mt-10"> 
-                                    Hi, I&apos;m Maks, frontend developer based in Poland. {/* Add GSAP aniumatoons on text */}
+                                    {/* Hi, I&apos;m Maks, frontend developer based in Poland. */} {/* Add GSAP aniumatoons on text */}
                                 </p>
 
                                 <a href="#about">
